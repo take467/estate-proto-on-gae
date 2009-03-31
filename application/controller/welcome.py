044a833a26e51745714355f28a0a752b452f5aac
Takeshi Fujisawa <takeshi.fujisawa@gmail.com>
2009-03-31 22:39:03 +0900

    refine treeview design

--- application/controller/welcome.py
+++ application/controller/welcome.py
@@ -9,7 +9,8 @@ class WelcomeController(BaseController):
 
     def before_action(self):
       user = users.get_current_user()
-      self.groups = [{'id':'0','name':'全カテゴリ','views':[{'id':'0','name':'全データ'},{'id':'1','name':'2009年'}]}]
+      #self.groups = [{'id':'0','name':'全カテゴリ','views':[{'id':'0','name':'全データ'},{'id':'1','name':'2009年'}]}]
+      self.groups = []
       for c in db.GqlQuery("SELECT * FROM ProfileGroup WHERE user = :1",user):
         self.groups.append({'id':c.key().id(),'name':c.name})
 
